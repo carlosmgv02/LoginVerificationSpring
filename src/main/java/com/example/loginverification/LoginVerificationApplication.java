a9afe5c2659da80b5ea9e65822594410b9cb537f
Carlos Martínez <carlos.martinezg@estudiants.urv.cat>
2022-11-19 19:33:30 +0100

    INITIAL FILES ADDED

---
+++ src/main/java/com/example/loginverification/LoginVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginVerificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginVerificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loginverification/LoginVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginVerificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
